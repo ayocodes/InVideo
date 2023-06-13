8f50eb26d14ab2a38871bfbe08a03709b2f83010
Ayo <omotolatomi01@gmail.com>
2023-06-13 20:06:15 +0100

    feat: work on main page

--- invideo-frontend/src/app/[Link]/page.tsx
+++ invideo-frontend/src/app/[Link]/page.tsx
@@ -9,17 +9,37 @@ const result = [
 
 const page = () => {
   return (
-    <main className=" py-[50px] px-[45px] h-screen">
+    <main className=" py-[50px] px-[45px]">
       <p className="">Crystalrohr</p>
-      <div className=" w-full h-full  flex justify-center">
+      <div className=" w-full h-full flex items-center flex-col gap-[70px] px-[100px]">
         <Link
-          className=" p-2.5 bg-[#81818163] flex  h-min gap-2.5 rounded-md"
+          className=" p-2.5 bg-[#242729] flex  h-min gap-2.5 rounded-md"
           href={"/"}
         >
           <img src="/arrow-left.svg" alt="" className=" " />
-          <p className=" text-sm">Go back</p>
+          <p className=" text-sm whitespace-nowrap">Go back</p>
         </Link>
-        
+        <div className=" grid grid-cols-3 w-full gap-[60px]">
+          {result.map((item, i) => (
+            <div key={i} className=" flex flex-col gap-5">
+              <img src={item.img} alt="" className=" h-[250px]" />
+              <p>Time stamp {item.time}</p>
+            </div>
+          ))}
+        </div>
+        <div className="bg-[#242729] w-full p-4 rounded-2xl">
+          <input
+            type="text"
+            className=" bg-[#171717] outline-none w-full p-2.5 rounded-2xl text-sm"
+            placeholder="Enter your question.."
+          />
+          <p className=" text-sm leading-7 mt-6 pb-[30px]">
+            These titles provide a general idea of the content and purpose of
+            each worksheet, allowing you to organize and manage different
+            aspects of your data effectively. Feel free to customize them based
+            on your specific needs and use cases.
+          </p>
+        </div>
       </div>
     </main>
   );
